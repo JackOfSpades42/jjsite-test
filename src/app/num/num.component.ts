7b6775bf041b38e618079e013cc6d9f177a58e14
JackOfSpades42 <jonjazdzewski@gmail.com>
2020-07-15 12:34:44 -0700

    button works

--- src/app/num/num.component.ts
+++ src/app/num/num.component.ts
@@ -14,7 +14,10 @@ export class NumComponent implements OnInit {
     //textbox.innerHTML += "I am thinking of a 5 digit number between 00000 and 99999<br>You input a number<br>and I will tell you how many are in the proper position and if your guess is an optimal guess.";
     textbox.innerHTML += "Please write down 5 seperate digits between 0 and 9.<br>I will show a list of numbers<br>and you tell me how many numbers I got in the right position.<br>If necessary you may use the spaces on the right to input your digits, but these spaces will not be used to choose my guesses (pinky promise)";
     let bottomText = document.getElementById("bottomText");
-    bottomText.innerHTML += "How many did I get right?<br><br><input type=\"number\" min=\"0\" max=\"5\"> <button ng-onclick=\"go\">Go!</button>";
+    bottomText.innerHTML += "How many did I get right?<br><br>";
   }
-
+  go(){
+    let textbox = document.getElementById("textbox");
+    textbox.innerHTML += "go!<br>";
+  }
 }
