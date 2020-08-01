1c80e6c06d9890acf6d29b2062c796a829899f04
JackOfSpades42 <jonjazdzewski@gmail.com>
2020-08-01 04:26:48 -0700

    Fix menu

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,11 +11,11 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.img2 = new Image;
+    /*this.img2 = new Image;
     this.img2.src = "assets/img/20200730_190312.jpg";
     this.img2.class = "portrait";
-    document.body.append(this.img2);
-    this.img = "/assets/img/20200730_190312.jpg";
+    document.body.append(this.img2);*/
+    this.img = "assets/img/20200730_190312.jpg";
   }
 
 }
