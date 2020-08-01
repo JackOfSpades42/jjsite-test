a490847a8552f8ab971857dd3dd7d8f5c1711978
JackOfSpades42 <jonjazdzewski@gmail.com>
2020-07-31 21:39:01 -0700

    imgs not working

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  img;
+  img2;
   constructor() { }
 
   ngOnInit() {
+    this.img2 = new Image;
+    this.img2.src = "assets/img/20200730_190312.jpg";
+    this.img2.class = "portrait";
+    document.body.append(this.img2);
+    this.img = "/assets/img/20200730_190312.jpg";
   }
 
 }
