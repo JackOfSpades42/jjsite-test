405cb62e85fbc71c6eee897dacbf07b374afcb6f
JackOfSpades42 <jonjazdzewski@gmail.com>
2020-08-17 03:24:07 -0700

    remove service call from chess

--- src/app/chess/chess.component.ts
+++ src/app/chess/chess.component.ts
@@ -1,21 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Xavier } from './chess.service';
-
 @Component({
   selector: 'app-chess',
   templateUrl: './chess.component.html',
   styleUrls: ['./chess.component.css'],
-  providers: [ Xavier ]
 })
 export class ChessComponent implements OnInit {
 
   constructor(
-    private Xavier : Xavier
   ) { };
 
   ngOnInit() {
-    //this.Xavier.doThing();
   }
 
 }
