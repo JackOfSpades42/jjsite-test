368c6de427e43919c93c59642b8c331a39ec8795
unknown <JonJazdzewski@gmail.com>
2020-09-11 04:32:05 -0700

    expand board

--- src/app/picross/picross.component.ts
+++ src/app/picross/picross.component.ts
@@ -16,12 +16,7 @@ export class PicrossComponent implements OnInit {
     for (let a=0;a<5;a++){
       this.board.rows[a] = new row;
       for (let b=0;b<5;b++){
-        this.board.rows[a].squares[b] = 1;
-        if (b==0){
-          this.board.rows[a].squares[b] = 0;
-        } else if (b==1){
-          this.board.rows[a].squares[b] = 0;
-        } else {}
+        this.board.rows[a].squares[b] = 2;
       }
     }
   }
