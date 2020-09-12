81c49bb174ef8bcc3279868c6ce5a39394570850
unknown <JonJazdzewski@gmail.com>
2020-09-12 02:36:18 -0700

    picross with input issues

--- src/app/picross/picross.component.ts
+++ src/app/picross/picross.component.ts
@@ -8,29 +8,101 @@ import { Component, OnInit } from '@angular/core';
 
 export class PicrossComponent implements OnInit {
 
-  board = new board;
+  board: board;
 
   constructor() { }
 
   ngOnInit(): void {
-    for (let a=0;a<5;a++){
-      this.board.rows[a] = new row;
-      for (let b=0;b<5;b++){
-        this.board.rows[a].squares[b] = 2;
-      }
+    this.board = new board(10);
+  }
+
+  solve(){
+    while (!this.board.solved){
+      
     }
   }
 
 }
+
 class row {
   squares: any[];
-  constructor(){
+  clues: number[];
+  filled: boolean;
+  reducedRows: reducedRow[];
+
+  constructor(a:number){
     this.squares = [];
+    for (let b=0;b<a;b++){
+      this.squares[b] = 2;
+    }
+    this.clues = [];
+    this.filled = false;
+  }
+
+  fill_start(){
+    for (let a=0;a<this.clues[0];a++){
+      this.squares[a] = 1;
+    }
+    this.squares[this.clues[0]] = 0;
+  }
+
+  fill_end(){
+    for(let a=this.squares.length-1;a>this.squares.length - this.clues[0];a--){
+      this.squares[a] =1;
+    }
+    this.squares[this.squares.length - this.clues[0] - 1] = 0;
+  }
+
+  reduce(){
+    if (this.clues.length>1){
+      for (let a=0;a<this.clues.length;a++){
+        let index1 = 0;
+        if (a>0){
+          index1 += a;
+          let b =0;
+          while(b < a){
+            index1 += this.clues[b];
+            b++;
+          }
+        }
+        let index2 = this.squares.length - 1;
+        if (a< this.clues.length-1){
+          index2 -= (this.squares.length - 1) - (this.clues.length - 1 - a);
+          let c =0;
+          while (c>a){
+            index2 -= this.clues[c];
+            c--;
+          }
+        }
+        this.reducedRows[a] = new reducedRow(this.clues[a],index1,index2,this);
+      }
+    }
   }
 }
+
+class reducedRow extends row {
+  index1: number;
+  index2: number;
+  targetClue: number;
+  parent: row;
+  constructor(clue,index1,index2,row){
+    super(row.squares.length);
+    this.targetClue = clue;
+    this.index1 = index1;
+    this.index2 = index2;
+    this.parent = row;
+  }
+}
+
+
 class board {
+    solved: boolean;
     rows: row[];
-    constructor(){
+    constructor(a:number){
       this.rows = [];
+      this.solved = false;
+      for (let b=0;b<a;b++){
+        this.rows[b] = new row(a);
+      }
     }
 }
