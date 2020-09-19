d165773d8e1bc5843b8e94f61e871656abeb8739
unknown <JonJazdzewski@gmail.com>
2020-09-19 08:10:55 -0700

    iterate function

--- src/app/picross/picross.component.ts
+++ src/app/picross/picross.component.ts
@@ -28,29 +28,56 @@ export class PicrossComponent implements OnInit {
   ngOnInit(): void {
     this.board = new board(5);
     this.squareSize = 5;
+    this.testClues();
+  }
+
+  testClues(){
+    this.picross.controls["col0"].setValue("2");
+    this.picross.controls["col1"].setValue("2");
+    this.picross.controls["col2"].setValue("3 1");
+    this.picross.controls["col3"].setValue("2 2");
+    this.picross.controls["col4"].setValue("3");
+    this.picross.controls["row0"].setValue("2");
+    this.picross.controls["row1"].setValue("2");
+    this.picross.controls["row2"].setValue("1 1 1");
+    this.picross.controls["row3"].setValue("5");
+    this.picross.controls["row4"].setValue("3");
   }
 
   solve(){
     this.board = new board(5);
     this.getClues(this.board);
     this.initialize(this.board);
+    for (let brdCount=0;brdCount<this.squareSize*2;brdCount++){
+      if (brdCount<this.squareSize){
+        this.board.rows[brdCount].iterate();
+      } else {
+        this.board.updateColsFromRows(brdCount-this.squareSize);
+        this.board.columns[brdCount-this.squareSize].iterate();
+        for (let update=0;update<this.squareSize;update++){
+          this.board.updateRowsFromCols(update);
+        }
+      }
+    }
     /*while (!this.board.solved){
       //
     }*/
-    //console.log(this.board.rows[0]);
+    //console.log(this.board.columns[1].reducedRows);
     let testRow = new row(5);
     testRow.clues = [2];
-    testRow.squares = [2,2,2,1,2];
-    /*testRow.reduce();
-    testRow.reducedRows[0].fill_overlap();
-    testRow.reducedRows[1].fill_overlap();
-    testRow.updateRowFromReduced();*/
-    testRow.fill_overlap();
-    testRow.fill_end();
+    testRow.squares = [2,2,0,1,2];
+    //testRow.reduce();
+    //testRow.reducedRows[0].fill_overlap();
+    //testRow.reducedRows[1].fill_overlap();
+    //testRow.updateRowFromReduced();
+    //testRow.fill_overlap();
+    //testRow.fill_end();
+    //testRow.fill_start();
+    testRow.iterate();
     console.log("testrow= " + testRow.squares);
   }
   onSubmit(){
-    console.log(this.picross.value);
+    //console.log(this.picross.value);
     if (this.picross.value.col0 > 10){
       let col0cntrl = this.picross.controls["col0"];
       col0cntrl.setValue("10");
@@ -85,6 +112,7 @@ export class PicrossComponent implements OnInit {
       brd.columns[a].clues.reverse();
     }
   }
+
   initialize(brd:board){
     for (let count=0;count<this.squareSize;count++){
       let row = brd.rows[count];
@@ -120,6 +148,10 @@ export class PicrossComponent implements OnInit {
       let col = brd.columns[colCount];
       if (col.clues.length>1){
         col.reduce();
+        if (colCount==1){
+          console.log(brd.columns[1].squares);
+          console.log(brd.columns[1].reducedRows);
+        }
         for (let colRed=0;colRed<col.reducedRows.length;colRed++){
           let currRed = col.reducedRows[colRed];
           currRed.fill_overlap();
@@ -237,7 +269,47 @@ class row {
       }
     }
   }
-}
+
+  iterate(){
+    let cluesFilled = 0;
+      if (!this.isFull()){
+        for (let b=0;b<this.squares.length;b++){
+          if (this.squares[b]==1){
+            if (b==this.squares.indexOf(1)){
+              if (b==this.clues[0]){
+                this.squares[0] = 0;
+              } else if (b<this.clues[0]-1){
+                this.squares[b+1] = 1;
+              }
+            }
+            if (this.squares[b-1]==0){
+              for (let c=b+1;c<this.clues[0] + b;c++){
+                this.squares[c] = 1;
+              }
+            }
+            if(b!==0 && b!==this.squares.length -1){
+              if (this.squares[b-1]==1){
+                let lineCount = 0;
+                let curr = b;
+                while (curr!==-1){
+                  if (this.squares[curr]==1){
+                    lineCount++;
+                  }
+                  curr--;
+                }
+                if (lineCount==this.clues[0]){
+                  cluesFilled++;
+                  while (this.squares.indexOf(2)!==-1){
+                    this.squares[this.squares.indexOf(2)] = 0;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 
 class reducedRow extends row {
   index1: number;
@@ -250,16 +322,39 @@ class reducedRow extends row {
     this.index1 = index1;
     this.index2 = index2;
     this.parent = row;
+    for (let a=0;a<this.squares.length;a++){
+      this.squares[a] = this.parent.squares[index1 + a];
+    }
+    this.reReduce();
   }
 
   fill_overlap(){
     if (this.squares.length/this.targetClue < 2){
       let edgeEmpties = this.squares.length - this.targetClue;
       for (let a=0;a<this.squares.length;a++){
-        if (a>=edgeEmpties || a<this.squares.length - edgeEmpties){ //double check this manually
+        if (a>=edgeEmpties && a<this.squares.length - edgeEmpties){
           this.squares[a]=1;
         }
       }
+    }
+    if (this.squares.length==this.targetClue){
+      if (this.index1!==0){
+        this.parent.squares[this.index1-1] = 0;
+      }
+      if (this.index2!==this.parent.squares.length-1){
+        this.parent.squares[this.index2+1] = 0;
+      }
+    }
+  }
+
+  reReduce(){
+    while(this.squares[0]==0){
+      this.squares.shift();
+      this.index1++;
+    }
+    while(this.squares[this.squares.length-1]==0){
+      this.squares.pop();
+      this.index2--;
     }
   }
 }
